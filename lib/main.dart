3f769579a6f2e3458c1593203aa485f71e323eca
Rohit <RohitSinghEmail@protonmail.com>
2022-07-28 22:08:07 +0530

    share to app when not in memory

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,21 @@ import 'package:dir_book/bookmark_storage/bookmarks_storage.dart';
 import 'package:dir_book/mainPage/mainPage.dart';
 import 'package:dir_book/theme/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
-void main() {
-  runApp(const MyApp());
+// Widget? root;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Widget root = await routeToCorrectPage();
+  runApp(MyApp(
+    home: root,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  Widget? home;
+  MyApp({Key? key, required this.home}) : super(key: key);
 
   //This widget is the root of your application.
   @override
@@ -20,9 +28,10 @@ class MyApp extends StatelessWidget {
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
       title: 'Flutter Demo',
-      home: MyHomePage(
-        bmStorage: BookMarkStorage(),
-      ),
+      // home: MyHomePage(
+      //   bmStorage: BookMarkStorage(),
+      // ),
+      home: home,
     );
   }
 }
@@ -53,4 +62,34 @@ class _MyHomePageState extends State<MyHomePage> {
       parentRoot: "/",
     );
   }
+}
+
+class SharePage extends StatefulWidget {
+  final String sharedLink;
+  SharePage({super.key, required this.sharedLink});
+  @override
+  _SharePageState createState() => _SharePageState();
+}
+
+class _SharePageState extends State<SharePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Share link"),
+      ),
+      body: Container(
+        child: Text("Shared Link is ${widget.sharedLink}"),
+      ),
+    );
+  }
+}
+
+Future<Widget> routeToCorrectPage() async {
+  String? sharedValue = await ReceiveSharingIntent.getInitialText();
+  if (sharedValue != null) {
+    return SharePage(sharedLink: sharedValue);
+  }
+
+  return MyHomePage(bmStorage: BookMarkStorage());
 }
